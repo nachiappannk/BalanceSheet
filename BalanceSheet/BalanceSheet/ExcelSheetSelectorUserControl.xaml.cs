b6bc3fa78eaca9fe8afe422198922c9bbed75d51
Nachiappan <nachiappan.nk.ext@sivantos.com>
2018-03-14 16:54:40 +0530

    Minor Clean up

--- BalanceSheet/BalanceSheet/ExcelSheetSelectorUserControl.xaml.cs
+++ BalanceSheet/BalanceSheet/ExcelSheetSelectorUserControl.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 
-namespace Nachiappan.BalanceSheetViewModel
+namespace Nachiappan.BalanceSheet
 {
     /// <summary>
     /// Interaction logic for ExcelSheetSelectorUserControl.xaml
