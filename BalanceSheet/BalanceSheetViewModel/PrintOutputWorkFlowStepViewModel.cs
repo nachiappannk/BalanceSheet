cb913a7bfb9ccb5c6ba515bc53fe1a7aab4a05f5
Nachiappan <nachiapan@gmail.com>
2018-03-17 22:10:05 +0530

    Added printing output file

--- BalanceSheet/BalanceSheetViewModel/PrintOutputWorkFlowStepViewModel.cs
+++ BalanceSheet/BalanceSheetViewModel/PrintOutputWorkFlowStepViewModel.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
 using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
 
 namespace Nachiappan.BalanceSheetViewModel
 {
     public class PrintOutputWorkFlowStepViewModel : WorkFlowStepViewModel
     {
+        private readonly DataStore _dataStore;
+        public DelegateCommand SaveOutputCommand { get; set; }
+
+        public InteractionRequest<FileSaveAsNotification> SaveOutputRequest { get; private set; }
+
         public PrintOutputWorkFlowStepViewModel(DataStore dataStore, Action goToPrevious)
         {
+            _dataStore = dataStore;
             GoToPreviousCommand = new DelegateCommand(goToPrevious);
             GoToNextCommand = new DelegateCommand(CloseApplication);
+            SaveOutputCommand = new DelegateCommand(SaveOutput);
+            SaveOutputRequest = new InteractionRequest<FileSaveAsNotification>();
+
 
         }
 
@@ -16,5 +29,40 @@ namespace Nachiappan.BalanceSheetViewModel
         {
             System.Windows.Application.Current.Shutdown();
         }
+
+        private void SaveOutput()
+        {
+            SaveFile("Save Output Document",
+                "FinancialReport",
+                ".xlsx",
+                "Excel File (.xlsx)|*.xlsx");
+        }
+
+        private void SaveFile(string saveFileTitle, string defaultFileName, string outputFileExtention, string filter)
+        {
+            var file = new FileSaveAsNotification()
+            {
+                Title = saveFileTitle,
+                DefaultFileName = defaultFileName,
+                OutputFileExtention = outputFileExtention,
+                OutputFileExtentionFilter = filter,
+            };
+
+            SaveOutputRequest.Raise(file);
+            if (file.FileSaved)
+            {
+                //file.OutputFileName
+                WriteJournal(file.OutputFileName);
+            }
+        }
+
+
+        private void WriteJournal(String fileName)
+        {
+
+            var journalStatements = _dataStore.GetPackage<List<JournalStatement>>();
+            JournalGateway gateway = new JournalGateway(fileName);
+            gateway.WriteJournal(journalStatements);
+        }       
     }
 }
