377f10ec0ba9b637de21f2bcd857680654080dbe
Nachiappan <nachiapan@gmail.com>
2018-03-25 01:09:18 +0530

    Clean balance sheet better

--- BalanceSheet/BalanceSheetViewModel/Model/ExcelGateway/BalanceSheetStatementsCleaner.cs
+++ BalanceSheet/BalanceSheetViewModel/Model/ExcelGateway/BalanceSheetStatementsCleaner.cs
@@ -11,13 +11,38 @@ namespace Nachiappan.BalanceSheetViewModel.Model.ExcelGateway
             List<BalanceSheetStatement> statements
             , ILogger logger)
         {
+
+            var trimmedStatements1 = RemoveDuplicateStatements(statements);
+
+            var trimmedStatements2 = RemoveStatementsWithInvalidAccount(statements, logger);
+
+            trimmedStatements2.AddRange(trimmedStatements1);
+
+            if (trimmedStatements2.Any())
+                logger.Log(MessageType.Warning, "Invalid statements from previous balance sheet are removed");
+            return trimmedStatements2;
+        }
+
+        private static List<TrimmedBalanceSheetStatement> RemoveStatementsWithInvalidAccount(List<BalanceSheetStatement> statements, ILogger logger)
+        {
             var mismatchedStatements = statements.Where(x => !AccountClassifer.IsRealLedger(x.Account)).ToList();
             statements.RemoveAll(x => mismatchedStatements.Contains(x));
-
-            if(mismatchedStatements.Any()) logger.Log(MessageType.Warning, "Invalid statements from previous balance sheet are removed");
-
-            return mismatchedStatements.Select(x => new TrimmedBalanceSheetStatement(x, "Invalid account name"))
+            
+            var z = mismatchedStatements.Select(x => new TrimmedBalanceSheetStatement(x, "Invalid account name"))
                 .ToList();
+            return z;
+        }
+
+        private static List<TrimmedBalanceSheetStatement> RemoveDuplicateStatements(List<BalanceSheetStatement> statements)
+        {
+            var accountToTimesDictionary =
+                statements.Select(x => x.Account).GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var invalidAccountNames = accountToTimesDictionary.Where(x => x.Value > 1).Select(x => x.Key).ToList();
+            var statementsWhereAccountIsrepeated = statements.Where(x => invalidAccountNames.Contains(x.Account)).ToList();
+            statements.RemoveAll(x => statementsWhereAccountIsrepeated.Contains(x));
+            var trimmedStatements = statementsWhereAccountIsrepeated.Select(x =>
+                new TrimmedBalanceSheetStatement(x, "The account entry is there multiple times")).ToList();
+            return trimmedStatements;
         }
     }
 }
